9589c7605c65fa1ec197daa9a9b66cf505ed4a0c
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2022-03-21 22:12:11 -0300

    Add files via upload
    Sorteio updated

--- Sorteio.java
+++ Sorteio.java
@@ -7,19 +7,30 @@ public class Sorteio {
         Random gerador = new Random();
         int c = 1;
         int num;
+        int soma = 0;
+        double media = 0;
+        int maxValue;
+        int minValue;
 
         // while (c <= 1000) {
         //     num = 1 + gerador.nextInt(100);
+        //     soma += num;
         //     System.out.println(num);
         //     c++;
         // }
 
+        // media = soma / 1000;
+        // System.out.println( "Média: " + media);
+
         for (c = 1; c <= 1000; c++) {
             num = 1 + gerador.nextInt(100);
+            soma += num;
             System.out.println(num);
             c++;
         }
-    
+        
+        media = soma / 1000;
+        System.out.println( "Média: " + media);
     }
 
 }
